b05b52256f8ba5ad4d12f2b104189fb1ad4b553f
Yusufpek <yusuf55ipek55@gmail.com>
2023-06-13 16:44:21 +0300

    dog image and name widget extracted

---
+++ lib/Widgets/ImageNameWidget.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+import '../Models/Dog.dart';
+
+class ImageNameWidget extends StatelessWidget {
+  final Dog dog;
+  const ImageNameWidget({super.key, required this.dog});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Image.network(dog.imageLink),
+        ),
+        const SizedBox(height: 16),
+        Text(dog.name, style: Theme.of(context).textTheme.bodyMedium),
+      ],
+    );
+  }
+}
