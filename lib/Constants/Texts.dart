72cae03e4614b24e7f455afb76f94baf197ee141
Yusufpek <yusuf55ipek55@gmail.com>
2023-06-13 16:43:11 +0300

    new texts added

--- lib/Constants/Texts.dart
+++ lib/Constants/Texts.dart
@@ -2,4 +2,16 @@
 
 class Texts {
   static const String appText = "Köpek Kütüphanesi";
+
+  static const String dogHeroTag = "dog-image-name-tag";
+
+  // Rating titles
+  static const String _communication = "İletişim:";
+  static const String goodWithChildren = "Çocuklarla $_communication";
+  static const String goodWithOtherDogs = "Diğer Köpeklerle $_communication";
+  static const String goodWithStrangers = "Yabancılarla $_communication";
+
+  static const String coatLength = "Tüy Uzunluğu";
+  static const String shedding = "Tüy Dökme";
+  static const String grooming = "Tüy Bakımı";
 }
