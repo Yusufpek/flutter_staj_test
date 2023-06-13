0b2537db9b4f2acc13b87f41685e730674c07596
Yusufpek <yusuf55ipek55@gmail.com>
2023-06-13 16:43:03 +0300

    custom images added

---
+++ lib/Constants/CustomImages.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: file_names
+
+class CustomImages {
+  static const String _base = "assets/";
+  static const String _type = ".png";
+  static const String pawEmptyImage = "${_base}paw_empty$_type";
+  static const String pawFullImage = "${_base}paw_full$_type";
+}
