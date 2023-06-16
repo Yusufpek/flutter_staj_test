2e0aa735b5ab4409a06c23a03a9c1bfc10cb8345
Yusufpek <yusuf55ipek55@gmail.com>
2023-06-16 15:35:33 +0300

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:staj_test/Pages/MainPage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,13 +9,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-      builder: (context, child) {
-        return DefaultTextStyle(
-          style: TextStyle(color: Colors.black),
-          child: child!,
-        );
-      },
+      home: const MainPage(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.orange,
+        primaryColor: Colors.orange,
+        cardColor: Colors.white,
+        dividerColor: Colors.orange,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.amber,
+        ),
+        textTheme: const TextTheme(
+            titleMedium: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+            bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+      ),
     );
   }
 }
